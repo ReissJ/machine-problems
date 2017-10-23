035cf56a18a98a16243c1f69cb45db01acb3d184
MarionFC <marionfcaccam@gmail.com>
2017-10-23 23:34:25 +0800

    machine problems

---
+++ No_4/No_4.py
@@ -0,0 +1,42 @@
+from PIL import Image
+from cImage import *
+
+def sepia(oldPixel):
+    red = oldPixel.getRed()
+    green = oldPixel.getGreen()
+    blue = oldPixel.getBlue()
+    newRed = (red * 0.393 + green * 0.769 + blue * 0.189)
+    newGreen = (red * 0.349 + green * 0.686 + blue * 0.168)
+    newBlue = (red * 0.272 + green * 0.534 + blue * 0.131)
+    newPixel = Pixel(newRed, newGreen, newBlue)
+    return newPixel
+
+def generalScale(oldimage, widthscale, heightscale):
+    oldw = oldimage.getWidth()
+    oldh = oldimage.getHeight()
+    newim = EmptyImage(oldw * widthscale, oldh * heightscale)
+
+    for row in range(newim.getHeight()):
+        for col in range(newim.getWidth()):
+            originalCol = col//widthscale
+            originalRow = row//heightscale
+            oldpixel = oldimage.getPixel(originalCol, originalRow)
+            newim = setPixel(col, row, oldpixel)
+    return newim
+
+def originalScale(oldimage):
+    for row in range(oldimage.getHeight):
+        for col in range(newim.getWidth()):
+            oldpixel = oldimage.getPixel(col, row)
+            sepieImage = sepia(oldpixel)
+            oldimage.setPixel(col, row, sepieImage)
+        return oldimage
+
+def displayImage(Image):
+    empWin = ImageWin("Image Processing", Image.getWidth(), Image.getHeight())
+    Image.draw(empWin)
+
+img = FileImage('toothless.jpg')
+sepia = originalScale(img)
+new = generalScale(sepia, 1, 2)
+displayImage(new)
